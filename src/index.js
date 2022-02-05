eb62bccd49fcecf99ffc316ec7ce94ba71a6f47b
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-02-05 12:40:57 +0100

    Added Navbar

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App.jsx";
+import "./index.css";
+
+let element = <button class="btn btn-primary">Hello</button>;
+ReactDOM.render(<NavBar />, document.getElementById("root"));
